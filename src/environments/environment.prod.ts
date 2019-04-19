f8dde043c36cb383d96fbd1c91a704259b8544d0
Konstantin Malikov <kmalikov@dev-team.com>
2019-04-19 14:22:43 +0300

    version variable added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   domain: 'alpha.vios.network',
-  viosNetworkApi: 'http://data.vios.network/ods/api'
+  viosNetworkApi: 'http://data.vios.network/ods/api',
+  version: '__build_ver_placeholder__'
 };
