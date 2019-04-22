1b940aa8b9f89034dcf5f976382dc63d9595e747
Konstantin Malikov <kmalikov@dev-team.com>
2019-04-22 15:09:05 +0300

    Scripts and logging added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   template: `<router-outlet></router-outlet>`
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    console.log(`Build version: ${environment.version}`);
+  }
 }
