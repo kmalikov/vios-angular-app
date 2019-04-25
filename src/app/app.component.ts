9c2faa8f4b41f8edec14bd41ea5c07f01a0b878d
Konstantin Malikov <kmalikov@dev-team.com>
2019-04-25 18:04:18 +0300

    added functionality of recognizing environment and setting "vios.js" depending on env

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,12 @@ import {environment} from '../environments/environment';
 export class AppComponent implements OnInit {
   constructor() {
     console.log(`Build version: ${environment.version}`);
+    let link;
+    link = document.createElement('script');
+    link.id = 'id3';
+    link.type = 'application/javascript';
+    link.src = `https://data.vios.network/DAV/home/vios${environment.production ? '' : '/dev'}/js/vios.js`;
+    document.head.appendChild(link);
   }
 
   ngOnInit() {
