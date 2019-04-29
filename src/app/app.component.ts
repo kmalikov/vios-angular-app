e365a1ec91edf30c599210070376f82a199b0b94
Konstantin Malikov <>
2019-04-29 09:43:52 +0300

    Append external scripts update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,24 @@ import {environment} from '../environments/environment';
 export class AppComponent implements OnInit {
   constructor() {
     console.log(`Build version: ${environment.version}`);
-    let link;
-    link = document.createElement('script');
-    link.id = 'id3';
-    link.type = 'application/javascript';
-    link.src = `https://data.vios.network/DAV/home/vios${environment.production ? '' : '/dev'}/js/vios.js`;
-    document.head.appendChild(link);
   }
 
   ngOnInit() {
+    this.appendExternalScripts();
+  }
+
+  appendExternalScripts() {
+    let link;
+    link = document.createElement('script');
+    link.id = 'viosjs';
+    link.type = 'application/javascript';
+    link.src = `https://data.vios.network/DAV/home/vios${environment.production ? '' : '/dev'}/js/vios.js`;
+    document.head.appendChild(link);
+
+    link = document.createElement('script');
+    link.id = 'setSID';
+    link.type = 'application/javascript';
+    link.src = `https://api.ipify.org?format=jsonp&callback=setSID`;
+    document.head.appendChild(link);
   }
 }
