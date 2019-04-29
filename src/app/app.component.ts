adea029950ff65eb7f3dc638016822f0b40af9a8
Konstantin Malikov <>
2019-04-29 09:55:02 +0300

    Append css file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   template: `<router-outlet></router-outlet>`
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor() {
     console.log(`Build version: ${environment.version}`);
+  }
+
+  ngOnInit() {
+    this.appendExternalScripts();
+  }
+
+  appendExternalScripts() {
     let link;
     link = document.createElement('script');
-    link.id = 'id3';
+    link.id = 'viosjs';
     link.type = 'application/javascript';
     link.src = `https://data.vios.network/DAV/home/vios${environment.production ? '' : '/dev'}/js/vios.js`;
+    document.head.appendChild(link);
+
+    link = document.createElement('script');
+    link.id = 'setSID';
+    link.type = 'application/javascript';
+    link.src = `https://api.ipify.org?format=jsonp&callback=setSID`;
+    document.head.appendChild(link);
+
+    link = document.createElement('link');
+    link.id = 'vioscss';
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+    link.src = `https://data.vios.network/DAV/home/vios/css/vios.css`;
     document.head.appendChild(link);
   }
 }
